db0a481657b41061cc017dc3e48ee8c749e8c39a
saeedniko <s.nikoobin@gmail.com>
2025-04-20 20:54:49 +0200

    Update LED configuration and pins

--- software/test/src/main.cpp
+++ software/test/src/main.cpp
@@ -1,67 +1,73 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define NUM_LEDS 80
+#define NUM_LEDS 139
 CRGB leds[NUM_LEDS];
 
-int adcval;
+int vpval;
+int vnval;
 
-#define eFuse_SHDN    32
-#define DATA_ESP      14
-#define Button3       33
-#define eFuse_Fault   27
-#define LED_PIN1      19
-#define LED_PIN2      21
-#define LVL_GATE      12
-#define V_SNS         34
-#define I_SNS         25
+#define eFuse_SHDN 2
+#define DATA_ESP 12
+#define Button3 14
+#define eFuse_Fault 15
+#define LED_PIN1 19
+#define LED_PIN2 21
+#define LVL_GATE 27
+#define V_SNS 36
+#define I_SNS 39
 
-void setup() {
+void setup()
+{
   // Serial UART
   Serial.begin(115200);
   Serial.println("test");
 
   // LED library
   FastLED.addLeds<NEOPIXEL, DATA_ESP>(leds, NUM_LEDS);
+  randomSeed(analogRead(0)); // Seed randomness
 
   // generic gpio
-  pinMode(eFuse_SHDN,OUTPUT);
+  pinMode(eFuse_SHDN, OUTPUT);
   pinMode(eFuse_Fault, INPUT);
   pinMode(Button3, INPUT);
   pinMode(LED_PIN1, OUTPUT); // make led pin output mode
   pinMode(LED_PIN2, OUTPUT); // make led pin output mode
   pinMode(LVL_GATE, OUTPUT); // make led pin output mode
-  pinMode(V_SNS, ANALOG);
-  pinMode(I_SNS, ANALOG);
+  //pinMode(V_SNS, ANALOG);
+  //pinMode(I_SNS, ANALOG);
 
-  //always high
+  // always high
   digitalWrite(eFuse_SHDN, 0);
   digitalWrite(LED_PIN1, 1);
   digitalWrite(LED_PIN2, 1);
   digitalWrite(LVL_GATE, 1);
-
 }
-
 
 void loop() {
-for (int i = 0; i<NUM_LEDS;i++)
-{
-  leds[i] = CRGB::Orange; 
-  FastLED.show(); 
-  delay(500);
-}
+  // Random color
+  CRGB newColor = CRGB(random(0, 256), random(0, 256), random(0, 256));
 
-for (int i = (NUM_LEDS-1); i>=0;i--)
-{
-  leds[i] = CRGB::DarkCyan; 
-  FastLED.show(); 
-  delay(500);
-}
+  // Random brightness between 30 and 255
+  uint8_t brightness = random(30, 256);  
+  FastLED.setBrightness(brightness);
 
-adcval = analogRead(V_SNS);
+  // Random direction
+  bool topToBottom = random(0, 2);
 
-Serial.print("ADC Value = ");
-Serial.print(adcval);
+  if (topToBottom) {
+    for (int i = 0; i < NUM_LEDS; i++) {
+      leds[i] = newColor;
+      FastLED.show();
+      delay(20); // animation speed
+    }
+  } else {
+    for (int i = NUM_LEDS - 1; i >= 0; i--) {
+      leds[i] = newColor;
+      FastLED.show();
+      delay(20);
+    }
+  }
 
-  
+  delay(2000); // Wait before next change
 }
